6042245405278097af93b0e915feff6b5f386407
CarlLight67 <carlvincentdictaan@gmail.com>
2026-07-11 12:34:43 +0800

    updated with bit of animations and ui design

--- animations/ani.js
+++ animations/ani.js
@@ -5,3 +5,35 @@
       duration: 2000,
       easing: 'easeOutElastic(0.2, 5)',
 });
+
+anime({
+      targets: '.hive',
+      opacity: [0,2],
+      translateX: [-200, 0],
+      duration: 5000 ,
+      rotate: [360]  ,    
+      easing: 'easeOutElastic(0.2, 5)'
+});
+
+
+      
+      anime({
+            targets: `main`,
+            opacity: [-0.2,1],
+            easing: 'easeOutElastic(0.2, 5)',
+            duration: 6000,
+            translateY: [10,0],
+      
+            easing: 'easeOutElastic(0.2, 5)'
+      });
+
+      anime({
+            targets: `footer`,
+            opacity: [-0.2,1],
+            easing: 'easeOutElastic(0.2, 5)',
+            duration: 1000,
+            translateY: [5,0],
+      
+            easing: 'easeOutElastic(0.2, 5)'
+      });
+
