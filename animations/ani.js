d1203f2d477a1ab62bcb45a172fba84524980f40
CarlLight67 <carlvincentdictaan@gmail.com>
2026-07-12 07:59:46 +0800

    this is the new now m gonna make this fast and done today

--- animations/ani.js
+++ animations/ani.js
@@ -19,7 +19,7 @@ anime({
       
       anime({
             targets: `main`,
-            opacity: [-0.2,1],
+            opacity: [0,1],
             translateY: [100,0],
             easing: 'easeOutElastic(1, 5)',
             duration: 6000,
@@ -27,43 +27,36 @@ anime({
 
       anime({
             targets: `footer`,
-            opacity: [-0.2,1],
+            opacity: [0,1],
             translateY: [100,0],
             easing: 'easeOutElastic(0.2, 5)',
             duration: 9000
       });
       anime({
             targets: `.box1`,
-            opacity: [-0.2,1],
+            opacity: [0,1],
             translateX: [-100,0],
             easing: 'easeOutElastic(0.2, 5)',
             duration: 2500
       });
       anime({
             targets: `.box2`,
-            opacity: [-0.2,1],
+            opacity: [0,1],
             translateX: [-200,0],
             easing: 'easeOutElastic(0.2, 5)',
             duration: 2500
       });
       anime({
             targets: `.box3`,
-            opacity: [-0.2,1],
+            opacity: [0,1],
             translateX: [-300,0],
             easing: 'easeOutElastic(0.2, 5)',
             duration: 2500
       });
       anime({
-            targets: `.box3`,
-            opacity: [-0.2,1],
-            translateX: [-400,0],
-            easing: 'easeOutElastic(0.2, 5)',
-            duration: 2500
-      });
-      anime({
             targets: `.box4`,
-            opacity: [-0.2,1],
-            translateX: [-500,0],
+            opacity: [0,1],
+            translateX: [-400,0],
             easing: 'easeOutElastic(0.2, 5)',
             duration: 2500
       });
